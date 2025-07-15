02ba730476fbf6f8dcb7620949efde5ab2c55998
Vishal586 <128839643+Vishal586@users.noreply.github.com>
2025-07-15 11:52:15 +0530

    Add files via upload

---
+++ frontend/src/api/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const API_BASE = 'http://localhost:5000/api';
+
+export const fetchUsers = () => axios.get(`${API_BASE}/users`);
+export const addUser = (name) => axios.post(`${API_BASE}/users`, { name });
+export const claimPoints = (userId) => axios.post(`${API_BASE}/claim`, { userId });
+export const getLeaderboard = () => axios.get(`${API_BASE}/users/leaderboard`);
+export const getHistory = (userId) => axios.get(`${API_BASE}/users/history/${userId}`);
